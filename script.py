d878a7c93b10830a69a49f7ba7feed27ced32776
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-02-20 17:40:33 -0500

    Calculate rate of return

--- script.py
+++ script.py
@@ -7,3 +7,8 @@ amazon_prices = [1699.8, 1777.44, 2012.71, 2003.0, 1598.01, 1690.17, 1501.97, 17
 
 ebay_prices = [35.98, 33.2, 34.35, 32.77, 28.81, 29.62, 27.86, 33.39, 37.01, 37.0, 38.6, 35.93, 39.5]
 
+#Calculate rate of return
+def get_returns(prices):
+  for i in range(0, prices + 1):
+    returns.append(calculate_log_return(i))
+  return returns
